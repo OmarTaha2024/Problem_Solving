aec2fa6cacd5e3bfb42d8254eecd7eddf8ec7d0d
OmarTaha2024 <155027970+OmarTaha2024@users.noreply.github.com>
2024-12-12 01:17:52 +0200

    Given a name S. Print "Hello, (name)"

---
+++ ICPC Assiut University Community/Data Types/A. Say Hello With C++/main.cpp
@@ -0,0 +1,29 @@
+/**                         Documentation section
+_________________________________________________________________________________________________________________
+-----------------------------------------------------------------------------------------------------------------
+@name :  A. Say Hello With C++
+@author : Omar Mohamed El Sayed Taha
+@Link : https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/A
+
+
+__________________________________________________________________________________________________________________
+------------------------------------------------------------------------------------------------------------------
+**/
+
+
+#include <bits/stdc++.h>
+#define Taha ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define ll long long
+#define el "\n"
+#define sp " "
+
+using namespace std;
+int main()
+{
+     string s;
+    getline(cin, s);
+
+    cout << "Hello, " << s;
+
+    return 0;
+}
