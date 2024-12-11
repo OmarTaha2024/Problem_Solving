6850736c620fa495e2d99de1c19d86b981c04567
OmarTaha2024 <155027970+OmarTaha2024@users.noreply.github.com>
2024-12-12 01:26:38 +0200

    Add solution for basic input/output problem  - Implemented a program to read and print different data types (int, long long, char, float, double) from user input. - Used `cin` to capture input and `cout` to display the values. - Tested with various types of input to ensure correct formatting and output.  This solution handles standard input/output operations for multiple data types.

---
+++ ICPC Assiut University Community/Data Types/B. Basic Data Types/main.cpp
@@ -0,0 +1,37 @@
+/**                         Documentation section
+_________________________________________________________________________________________________________________
+-----------------------------------------------------------------------------------------------------------------
+@name :  B. Basic Data Types
+@author : Omar Mohamed El Sayed Taha
+@Link : https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/B
+
+
+__________________________________________________________________________________________________________________
+------------------------------------------------------------------------------------------------------------------
+**/
+
+
+#include <bits/stdc++.h>
+#define Taha ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define ll long long
+#define el "\n"
+#define sp " "
+
+using namespace std;
+int main()
+{
+    Taha ;
+    int a ;
+    long long b;
+    char c;
+    float f;
+    double d;
+    cin>>a>>b>>c>>f>>d;
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << f << endl;
+    cout << d << endl;
+
+    return 0;
+}
