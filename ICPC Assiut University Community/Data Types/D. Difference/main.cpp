d377edfe11d1de785b25a2dbf8c49c73ccda2e6c
OmarTaha2024 <155027970+OmarTaha2024@users.noreply.github.com>
2024-12-12 15:56:17 +0200

    Solve "D. Difference" problem    - Reads four integers.   - Calculates the difference between the product of the first two and the last two.   - Outputs the result in the format: "Difference = result".

---
+++ ICPC Assiut University Community/Data Types/D. Difference/main.cpp
@@ -0,0 +1,29 @@
+/**                         Documentation section
+_________________________________________________________________________________________________________________
+-----------------------------------------------------------------------------------------------------------------
+@name :  D. Difference
+@author : Omar Mohamed El Sayed Taha
+@Link : https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/D
+
+
+__________________________________________________________________________________________________________________
+------------------------------------------------------------------------------------------------------------------
+**/
+
+
+#include <bits/stdc++.h>
+#define Taha ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define ll long long
+#define el "\n"
+#define sp " "
+
+using namespace std;
+
+int main()
+{
+    Taha;
+    ll  a, b, c, d ;
+    cin>> a >> b >> c >> d ;
+    cout<< "Difference = " <<( a * b ) - ( c * d )<<endl;
+    return 0;
+}
