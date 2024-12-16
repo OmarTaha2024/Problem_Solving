35963257970af9c8f2647493944ce7bf8d2080a7
OmarTaha2024 <155027970+OmarTaha2024@users.noreply.github.com>
2024-12-16 15:53:08 +0200

    Implement a solution to check if the first number is greater than or equal to the second number and print 'Yes' if true, otherwise 'No

---
+++ ICPC Assiut University Community/Data Types/I. Welcome for you with Conditions/main.cpp
@@ -0,0 +1,26 @@
+/**                         Documentation section
+_________________________________________________________________________________________________________________
+-----------------------------------------------------------------------------------------------------------------
+@name :  I. Welcome for you with Conditions
+@author : Omar Mohamed El Sayed Taha
+@Link : https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/I
+
+__________________________________________________________________________________________________________________
+------------------------------------------------------------------------------------------------------------------
+**/
+
+#include <bits/stdc++.h>
+#define Taha ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define ll long long
+#define el "\n"
+#define sp " "
+using namespace std;
+
+int main()
+{
+    Taha;
+    int a, b;
+    cin >> a >> b;
+    cout << (a < b ? "No" : "Yes") << el;
+    return 0;
+}
