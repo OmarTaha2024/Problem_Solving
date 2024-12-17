22203253f9ca666de463ecc23cc07d2da4b8c2ea
OmarTaha2024 <155027970+OmarTaha2024@users.noreply.github.com>
2024-12-17 22:32:13 +0200

    Add solution to check if two individuals share the same family name and print 'ARE Brothers' if true, otherwise 'NOT'

---
+++ ICPC Assiut University Community/Data Types/L. The Brothers/main.cpp
@@ -0,0 +1,29 @@
+/**                         Documentation section
+_________________________________________________________________________________________________________________
+-----------------------------------------------------------------------------------------------------------------
+@name :  L. The Brothers
+@author : Omar Mohamed El Sayed Taha
+@Link : https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/L
+
+
+__________________________________________________________________________________________________________________
+------------------------------------------------------------------------------------------------------------------
+**/
+
+
+#include <bits/stdc++.h>
+#define Taha ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define ll long long
+#define el "\n"
+#define sp " "
+
+using namespace std;
+
+int main()
+{
+    Taha;
+    string s1,s2,s3,s4;
+    cin>>s1>>s2>>s3>>s4;
+    cout<< ((s2 == s4) ? "ARE Brothers" :"NOT")<<el;
+    return 0;
+}
