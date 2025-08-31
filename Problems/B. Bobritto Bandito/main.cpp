a02b0f9b71a33b681e55cc18589cd647536ab0a2
OmarTaha2024 <155027970+OmarTaha2024@users.noreply.github.com>
2025-08-31 20:25:41 +0300

    Solve B problem

--- Problems/B. Bobritto Bandito/main.cpp
+++ Problems/B. Bobritto Bandito/main.cpp
@@ -1,9 +1,37 @@
-#include <iostream>
+/**                         Documentation section
+_________________________________________________________________________________________________________________
+-----------------------------------------------------------------------------------------------------------------
+@name :   B. Bobritto Bandito
+@author : Omar Mohamed El Sayed Taha
+@Link : https://codeforces.com/contest/2094/problem/B
 
+
+
+__________________________________________________________________________________________________________________
+------------------------------------------------------------------------------------------------------------------
+**/
+
+
+#include <bits/stdc++.h>
+#define Taha ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define ll long long
+#define el "\n"
+#define sp " "
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    Taha;
+    int t ;
+    cin>> t ;
+    while(t--)
+    {
+        long long n, m, l, r;
+        cin >> n >> m >> l >> r;
+        long long a = min(abs(l), m);
+        long long L = -a;
+        long long R= m - a;
+        cout << L << " " << R << "\n";
+    }
     return 0;
 }
