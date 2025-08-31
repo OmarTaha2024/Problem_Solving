24560590b790b23fc21f5e840068c969ff52ee71
OmarTaha2024 <155027970+OmarTaha2024@users.noreply.github.com>
2025-08-31 20:07:06 +0300

    A problem solved

---
+++ Problems/A.Trippi Troppi/main.cpp
@@ -0,0 +1,36 @@
+/**                         Documentation section
+_________________________________________________________________________________________________________________
+-----------------------------------------------------------------------------------------------------------------
+@name :    A. Trippi Troppi
+@author : Omar Mohamed El Sayed Taha
+@Link : https://codeforces.com/contest/2094/problem/A
+
+
+
+__________________________________________________________________________________________________________________
+------------------------------------------------------------------------------------------------------------------
+**/
+
+
+#include <bits/stdc++.h>
+#define Taha ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define ll long long
+#define el "\n"
+#define sp " "
+using namespace std;
+
+int main()
+{
+    Taha;
+    int t ;
+    cin>> t ;
+    while(t--)
+    {
+        string s1 ;
+        string s2 ;
+        string s3 ;
+        cin>>s1>>s2>>s3;
+        cout<<s1[0]<<s2[0]<<s3[0]<<endl;
+    }
+    return 0;
+}
