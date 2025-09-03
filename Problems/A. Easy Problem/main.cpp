ff9fd5cf8a61da0d4542397da37e155bd7ca2371
OmarTaha2024 <155027970+OmarTaha2024@users.noreply.github.com>
2025-09-03 15:45:24 +0300

    solve A problem

---
+++ Problems/A. Easy Problem/main.cpp
@@ -0,0 +1,34 @@
+/**                         Documentation section
+_________________________________________________________________________________________________________________
+-----------------------------------------------------------------------------------------------------------------
+@name :    A. Easy Problem
+@author : Omar Mohamed El Sayed Taha
+@Link : https://codeforces.com/contest/2044/problem/A
+
+
+
+__________________________________________________________________________________________________________________
+------------------------------------------------------------------------------------------------------------------
+**/
+
+
+#include <bits/stdc++.h>
+#define Taha ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define ll long long
+#define el "\n"
+#define sp " "
+using namespace std;
+
+int main()
+{
+    Taha;
+    int t ;
+    cin>> t ;
+    while(t--)
+    {
+      int n ;
+      cin>>n;
+      cout<<n-1<<endl;
+    }
+    return 0;
+}
